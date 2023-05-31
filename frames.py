3f51adddb4585c19bcee46079e11d62d918a200f
josev <jdv72@cornell.edu>
2023-05-31 01:44:16 -0400

    Implemented colvolution :)

--- frames.py
+++ frames.py
@@ -10,6 +10,8 @@ import cv2
 from PIL import Image, ImageTk, ImageDraw
 from rembg import remove
 import image_editorUI
+import requests
+from io import BytesIO
 
 BUTTON_WIDTH = 1
 SLIDER_LENGTH = 250
@@ -54,22 +56,94 @@ class BaseFrame(tk.Frame):
         self.status.configure(text=text)
 
 
+class ImageGenerationFrame(BaseFrame):
+    def __init__(self, parent, root):
+        super().__init__(parent, root)
+
+        self.screenshotButton = tk.Button(self, text='Save Image',
+                                          command=self.screenshot, width=BUTTON_WIDTH)
+        self.screenshotButton.grid(row=0, column=5, sticky=tk.W+tk.E)
+
+        self.generateButton = tk.Button(
+            self, text='Generate Image', command=self.generateImage, width=BUTTON_WIDTH)
+        self.generateButton.grid(
+            row=0, column=0, sticky=tk.W+tk.E)
+
+        self.textEntry = tk.Entry(self)  # Create a text entry widget
+        # Adjust the row and column as needed
+        self.textEntry.grid(rowspan=2, columnspan=6,
+                            sticky=tk.N+tk.S+tk.E+tk.W)
+
+        self.imageCanvas.grid(row=3, columnspan=6, sticky=tk.N+tk.S+tk.E+tk.W)
+
+        self.status.grid(row=4, columnspan=6, sticky=tk.S)
+
+        self.image = None
+
+    def get_text(self):
+        return self.textEntry.get()
+
+    def generateImage(self):
+        # text = self.get_text()
+        # if (text == ''):
+        #     self.setStatus('Please write a description of the desired image')
+        # else:
+        #     self.setStatus('Generating Image...')
+        #     r = requests.post("https://api.deepai.org/api/text2img",
+        #                       data={
+        #                           'text': text,
+        #                       },
+        #                       headers={
+        #                           'api-key': 'b10bfb39-9d84-45e8-bc89-8f8f57c220e1',
+        #                           'grid_size': "1"}
+        #                       )
+        #     print(r.json())
+        #     response = r.json()
+        #     if 'output_url' in response:
+        #         image_url = response['output_url']
+        #         image_data = requests.get(
+        #             image_url).content  # Fetch the image data
+        #         # Open the image using PIL
+        #         image = Image.open(BytesIO(image_data))
+        #         # # Convert the image to a Tkinter-compatible format
+        #         # photo = ImageTk.PhotoImage(image)
+        #         photo = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        #         # self.imageCanvas.config(image=photo)  # Set the image in the label
+        #         # # Keep a reference to prevent image garbage collection
+        #         # self.imageCanvas.image = photo
+        #         self.imageCanvas.drawCVImage(photo)
+        #         self.setStatus('Image Generated Successfully')
+        #     else:
+        #         self.setStatus('Error Generating Image')
+        self.setStatus('Sorry, our image generation is out of order :()')
+
+    def screenshot(self):
+        if self.image is not None:
+            filename = tkFileDialog.asksaveasfilename(parent=self.root,
+                                                      filetypes=supportedFiletypes, defaultextension=".png")
+            if filename:
+                self.imageCanvas.writeToFile(filename)
+                self.setStatus('Saved image to ' + filename)
+        else:
+            image_editorUI.error('Load image before taking a screenshot!')
+
+
 class ConvolveFrame(BaseFrame):
     def __init__(self, parent, root):
         super().__init__(parent, root)
 
         self.loadImageButton = tk.Button(self, text='Load Image',
                                          command=self.loadImage, width=BUTTON_WIDTH)
-        self.loadImageButton.grid(row=0, column=0, sticky=tk.W+tk.E)
+        self.loadImageButton.grid(row=5, column=0, sticky=tk.W+tk.E)
 
         self.screenshotButton = tk.Button(self, text='Save Image',
                                           command=self.screenshot, width=BUTTON_WIDTH)
-        self.screenshotButton.grid(row=0, column=5, sticky=tk.W+tk.E)
+        self.screenshotButton.grid(row=5, column=5, sticky=tk.W+tk.E)
 
         self.convolveButton = tk.Button(
             self, text='Convolve', command=self.applyConvolution, width=BUTTON_WIDTH)
 
-        self.convolveButton.grid(row=0, column=1, sticky=tk.W+tk.E)
+        self.convolveButton.grid(row=5, column=1, sticky=tk.W+tk.E)
 
         self.imageCanvas.grid(row=3, columnspan=6, sticky=tk.N+tk.S+tk.E+tk.W)
 
@@ -83,9 +157,12 @@ class ConvolveFrame(BaseFrame):
             row_entries = []
             for j in range(3):
                 entry = tk.Entry(self)
-                entry.grid(row=i+1, column=j+1, padx=5, pady=5)
+                entry.grid(row=i, column=j+1, padx=5, pady=5)
                 row_entries.append(entry)
             self.matrix_entries.append(row_entries)
+
+        self.entrymult = tk.Entry(self)
+        self.entrymult.grid(row=1, column=0, padx=5, pady=5)
 
     def applyConvolution(self):
         # Retrieve the entered matrix values
@@ -106,7 +183,10 @@ class ConvolveFrame(BaseFrame):
             self.convolve(matrix)
 
     def convolve(self, matrix):
+
         kernel = np.array(matrix)
+        kernel = float(self.entrymult.get()) * kernel
+        print(kernel)
 
         convolved = cv2.filter2D(src=self.image, ddepth=-1, kernel=kernel)
         self.image = np.copy(convolved)
@@ -299,6 +379,10 @@ class ImageEditorFrame(tk.Frame):
         self.ConvolveFrame = ConvolveFrame(self.notebook, root)
 
         self.notebook.add(self.ConvolveFrame, text="Colvolve an Image")
+
+        self.ImageGenerationFrame = ImageGenerationFrame(self.notebook, root)
+
+        self.notebook.add(self.ImageGenerationFrame, text='Generate an Image')
 
         self.notebook.grid(row=0, sticky=tk.N+tk.S+tk.E+tk.W)
 
